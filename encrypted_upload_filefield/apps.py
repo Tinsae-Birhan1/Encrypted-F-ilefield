df2ac1a5b8752a921c5c1ad84522b037dd9a3809
tinsae-birhan1 <tinsaebirhan7@gmail.com>
2024-04-03 17:18:53 +0300

    first commit

---
+++ encrypted_upload_filefield/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EncryptedUploadFilefieldConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'encrypted_upload_filefield'
